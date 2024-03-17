a3cae0d8188cf008cbfdec2819bc27660ea89065
Kapil Badgujjar <k.b.cool.boy99@gmail.com>
2024-03-17 16:04:21 +0530

    Origin added

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
           {
             source: "/api/:path*", // Matches all API routes
             headers: [
-              { key: "Access-Control-Allow-Origin", value: "https://checkout.stripe.com" },
+              { key: "Access-Control-Allow-Origin", value: ["https://checkout.stripe.com","https://test-app-kapil-badgujjar.vercel.app"] },
               { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
               { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
             ],
